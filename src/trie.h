99453fe68a84b141431770118c3851bd014b6e17
Michael Clark <michael@metaparadigm.com>
2015-03-29 11:37:32 +0800

    Add prefix tree (intended for url matching)

---
+++ src/trie.h
@@ -0,0 +1,236 @@
+//
+//  trie.h
+//
+
+#ifndef trie_h
+#define trie_h
+
+struct trie_node;
+typedef std::vector<trie_node*> trie_node_list;
+
+enum trie_node_type
+{
+    trie_node_type_branch,
+    trie_node_type_leaf,
+};
+
+struct trie_node
+{
+    std::string         prefix;
+    
+    trie_node(std::string &prefix) : prefix(prefix) {}
+    virtual ~trie_node() {}
+    
+    virtual trie_node_type get_type() = 0;
+};
+
+template <typename T>
+struct trie_branch_node : trie_node
+{
+    trie_node_list    nodes;
+    
+    trie_node_type get_type() { return trie_node_type_branch; }
+
+    trie_branch_node(std::string prefix) : trie_node(prefix) {}
+};
+
+template <typename T>
+struct trie_leaf_node : trie_node
+{
+    T                   val;
+
+    trie_node_type get_type() { return trie_node_type_leaf; }
+    
+    trie_leaf_node(std::string prefix, T val) : trie_node(prefix), val(val) {}
+};
+
+template <typename T>
+struct trie
+{
+    trie_node* root_node;
+    
+    typedef std::pair<std::string,T> pair_type;
+    typedef std::vector<pair_type> vec_type;
+    typedef trie_leaf_node<T> leaf_type;
+    typedef trie_branch_node<T> branch_type;
+    
+    static struct {
+        bool operator()(const trie_node *lhs, const trie_node *rhs) { return lhs->prefix < rhs->prefix; }
+    } node_prefix_cmp;
+    
+    trie() : root_node(nullptr) {}
+    
+    static void sort_nodes(branch_type *node)
+    {
+        std::sort(node->nodes.begin(), node->nodes.end(), node_prefix_cmp);
+    }
+    
+    static void print(trie_node *node, size_t offset, int key_width, bool do_print)
+    {
+        int pad = (int)(offset + node->prefix.length());
+        if (node->get_type() == trie_node_type_leaf) {
+            auto lnode = static_cast<trie_leaf_node<T>*>(node);
+            if (do_print)
+                std::cout << std::setfill(' ') << std::setw(pad) << lnode->prefix
+                          << std::setw(key_width - pad) << " " << ":= " << lnode->val << std::endl;
+        }
+        if (node->get_type() == trie_node_type_branch) {
+            auto inode = static_cast<trie_branch_node<T>*>(node);
+            if (do_print)
+                std::cout << std::setfill(' ') << std::setw(pad) << inode->prefix
+                          << std::endl;
+            for (auto cnode : inode->nodes) {
+                print(cnode, offset + inode->prefix.length(), key_width, true);
+            }
+        }
+    }
+    
+    void print()
+    {
+        if (root_node) print(root_node, 0, 60, false);
+    }
+    
+    void find_node_internal(std::string &key, trie_node* &node, trie_node* &parent,
+                            size_t &prefix_offset, size_t &key_offset, size_t &child_index)
+    {
+        // p, i, c
+        prefix_offset = key_offset = child_index = 0;
+        while(key_offset < key.length()) {
+        next:
+            if (prefix_offset < node->prefix.length() &&
+                node->prefix[prefix_offset] == key[key_offset])
+            {
+                prefix_offset++; key_offset++;
+                continue;
+            }
+            else if (node->get_type() == trie_node_type_leaf)
+            {
+                if (prefix_offset == node->prefix.length()) {
+                    // matched to end of leaf
+                }
+                break;
+            }
+            else if (node->get_type() == trie_node_type_branch)
+            {
+                if (prefix_offset == node->prefix.length()) {
+                    // matched to end of branch
+                    branch_type* branch = static_cast<branch_type*>(node);
+                    child_index = 0;
+                    for (auto child_node : branch->nodes) {
+                        // matched child
+                        if (child_node->prefix.length() > 0 && child_node->prefix[0] == key[key_offset]) {
+                            prefix_offset = 0;
+                            parent = node;
+                            node = child_node;
+                            goto next;
+                        }
+                        child_index++;
+                    }
+                    break;
+                } else {
+                    // partial match
+                    break;
+                }
+            }
+        }
+    }
+    
+    bool insert(std::string key, T val)
+    {
+        if (!root_node) {
+            root_node = new branch_type("");
+            static_cast<branch_type*>(root_node)->nodes.push_back(new leaf_type(key, val));
+            return true;
+        }
+
+        // find nearest matching node
+        trie_node *node = root_node, *parent = nullptr;
+        size_t prefix_offset, key_offset, child_index;
+        find_node_internal(key, node, parent, prefix_offset, key_offset, child_index);
+        
+        // exact match, duplicate key
+        if (key.length() == key_offset) {
+            return false;
+        }
+        
+        // insert new leaf node
+        if (node->get_type() == trie_node_type_leaf)
+        {
+            // split leaf and create new branch
+            std::string parent_prefix = node->prefix.substr(0, prefix_offset);
+            std::string old_child_prefix = node->prefix.substr(prefix_offset);
+            std::string new_child_prefix = key.substr(key_offset);
+            branch_type* new_branch = new branch_type(parent_prefix);
+            leaf_type* old_leaf = new leaf_type(old_child_prefix, static_cast<leaf_type*>(node)->val);
+            leaf_type* new_leaf = new leaf_type(new_child_prefix, val);
+            new_branch->nodes.push_back(old_leaf);
+            new_branch->nodes.push_back(new_leaf);
+            sort_nodes(new_branch);
+            static_cast<branch_type*>(parent)->nodes[child_index] = new_branch;
+            delete node;
+        }
+        else if (node->get_type() == trie_node_type_branch)
+        {
+            if (prefix_offset == node->prefix.length())
+            {
+                // add new leaf
+                std::string new_child_prefix = key.substr(key_offset);
+                leaf_type* new_leaf = new leaf_type(new_child_prefix, val);
+                static_cast<branch_type*>(node)->nodes.push_back(new_leaf);
+                sort_nodes(static_cast<branch_type*>(node));
+            }
+            else if (prefix_offset < node->prefix.length())
+            {
+                // split branch, copy child nodes and add new leaf
+                std::string parent_prefix = node->prefix.substr(0, prefix_offset);
+                std::string old_child_prefix = node->prefix.substr(prefix_offset);
+                std::string new_child_prefix = key.substr(key_offset);
+                branch_type* new_branch = new branch_type(parent_prefix);
+                branch_type* old_branch = new branch_type(old_child_prefix);
+                for (auto child_node : static_cast<branch_type*>(node)->nodes) {
+                    old_branch->nodes.push_back(child_node);
+                }
+                leaf_type* new_leaf = new leaf_type(new_child_prefix, val);
+                new_branch->nodes.push_back(old_branch);
+                new_branch->nodes.push_back(new_leaf);
+                sort_nodes(new_branch);
+                static_cast<branch_type*>(parent)->nodes[child_index] = new_branch;
+                delete node;
+            }
+        }
+        
+        return true;
+    }
+    
+    bool find(std::string key, T &val)
+    {
+        // find nearest matching node
+        trie_node *node = root_node, *parent = nullptr;
+        size_t prefix_offset, key_offset, child_index;
+        find_node_internal(key, node, parent, prefix_offset, key_offset, child_index);
+        
+        // exact match
+        if (key.length() == key_offset) {
+            if (node->get_type() == trie_node_type_leaf) {
+                val = static_cast<leaf_type*>(node)->val;
+                return true;
+            } else if (node->get_type() == trie_node_type_branch) {
+                branch_type *branch = static_cast<branch_type*>(node);
+                // Check if first child node is sentinel ""
+                // Note: depends on nodes being sorted
+                if (branch->nodes.size() > 0 &&
+                    branch->nodes[0]->get_type() == trie_node_type_leaf &&
+                    branch->nodes[0]->prefix.size() == 0)
+                {
+                    val = static_cast<leaf_type*>(branch->nodes[0])->val;
+                    return true;
+                }
+            }
+        }
+        
+        // not found
+        return false;
+    }
+};
+
+#endif
---
+++ tests/test_trie.cc
@@ -0,0 +1,145 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <cerrno>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <chrono>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+#include "trie.h"
+
+
+void trie_add(trie<uint32_t> &trie, std::vector<std::string> &words)
+{
+    size_t i = 0;
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    for (auto word: words) {
+        trie.insert(word, (uint32_t)i++);
+    }
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    uint64_t total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    uint64_t item_ns = total_ns / words.size();
+    printf("%-20s items=(%9lu) total_time=(%12llu ns) item_time=(%6llu ns)\n",
+           __func__, words.size(), total_ns, item_ns);
+}
+
+void trie_lookup(trie<uint32_t> &trie, std::vector<std::string> &words)
+{
+    size_t i = 0;
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    for (auto word: words) {
+        uint32_t result;
+        if (trie.find(word, result)) i++;
+        else printf("%s couldn't find \"%s\"\n", __func__, word.c_str());
+    }
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    uint64_t total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    uint64_t item_ns = total_ns / i;
+    printf("%-20s items=(%9lu) total_time=(%12llu ns) item_time=(%6llu ns)\n",
+           __func__, i, total_ns, item_ns);
+}
+
+void map_add(std::map<std::string,uint32_t> &map, std::vector<std::string> &words)
+{
+    size_t i = 0;
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    for (auto word: words) {
+        map.insert(std::pair<std::string,uint32_t>(word, i));
+    }
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    uint64_t total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    uint64_t item_ns = total_ns / words.size();
+    printf("%-20s items=(%9lu) total_time=(%12llu ns) item_time=(%6llu ns)\n",
+           __func__, words.size(), total_ns, item_ns);
+}
+
+void map_lookup(std::map<std::string,uint32_t> &map, std::vector<std::string> &words)
+{
+    size_t i = 0;
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    for (auto word: words) {
+        auto mi = map.find(word);
+        if (mi != map.end()) i++;
+        else printf("%s couldn't find \"%s\"\n", __func__, word.c_str());
+    }
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    uint64_t total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    uint64_t item_ns = total_ns / i;
+    printf("%-20s items=(%9lu) total_time=(%12llu ns) item_time=(%6llu ns)\n",
+           __func__, i, total_ns, item_ns);
+}
+
+void unordered_map_add(std::unordered_map<std::string,uint32_t> &map, std::vector<std::string> &words)
+{
+    size_t i = 0;
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    for (auto word: words) {
+        map.insert(std::pair<std::string,uint32_t>(word, i));
+    }
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    uint64_t total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    uint64_t item_ns = total_ns / words.size();
+    printf("%-20s items=(%9lu) total_time=(%12llu ns) item_time=(%6llu ns)\n",
+           __func__, words.size(), total_ns, item_ns);
+}
+
+void unordered_map_lookup(std::unordered_map<std::string,uint32_t> &map, std::vector<std::string> &words)
+{
+    size_t i = 0;
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    for (auto word: words) {
+        auto mi = map.find(word);
+        if (mi != map.end()) i++;
+        else printf("%s couldn't find \"%s\"\n", __func__, word.c_str());
+    }
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    uint64_t total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    uint64_t item_ns = total_ns / i;
+    printf("%-20s items=(%9lu) total_time=(%12llu ns) item_time=(%6llu ns)\n",
+           __func__, i, total_ns, item_ns);
+}
+
+void read_words(std::vector<std::string> &words, std::string filename)
+{
+    char buf[1024];
+    FILE *file = fopen("/usr/share/dict/words", "r");
+    if (!file) {
+        fprintf(stderr, "error: %s", strerror(errno));
+        exit(0);
+    }
+    const auto t1 = std::chrono::high_resolution_clock::now();
+    char *line;
+    while((line = fgets(buf, sizeof(buf), file))) {
+        size_t len = strlen(line);
+        if (len > 0 && line[len-1] == '\n') line[len-1] = '\0';
+        words.push_back(line);
+    }
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    uint64_t total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    uint64_t item_ns = total_ns / words.size();
+    fclose(file);
+    printf("%-20s items=(%9lu) total_time=(%12llu ns) item_time=(%6llu ns)\n",
+           __func__, words.size(), total_ns, item_ns);
+}
+
+int main(int argc, char **argv)
+{
+    std::vector<std::string> words;
+    read_words(words, "/usr/share/dict/words");
+
+    trie<uint32_t> mt;
+    trie_add(mt, words);
+    trie_lookup(mt, words);
+
+    std::map<std::string,uint32_t> map;
+    map_add(map, words);
+    map_lookup(map,words);
+
+    std::unordered_map<std::string,uint32_t> unordered_map;
+    unordered_map_add(unordered_map, words);
+    unordered_map_lookup(unordered_map,words);
+}
