81e1b4540f1f2a82f6f8a00dd5f3b40faa18a3de
Michael Clark <michael@metaparadigm.com>
2015-03-31 12:43:41 +0800

    Update example application server

--- src/neta.cc
+++ src/neta.cc
@@ -4,15 +4,27 @@
 
 #include "latypus.h"
 
-/* neta */
+/* neta
+ *
+ * example application server (work in progress)
+ */
 
 int main(int argc, const char * argv[])
 {
     http_constants::init();
     protocol::init();
     
+    struct fn {
+        void operator()(http_server_connection *conn)
+        {
+            std::string request_path = conn->request.get_request_path();
+            //conn->response->set_response(200, "echo " + request_path);
+        }
+    };
+    
     protocol_engine engine;
     engine.default_config("http_server");
+    //engine.bind_function("http_server", "/echo/", fn());
     engine.run();
     engine.join();
     
