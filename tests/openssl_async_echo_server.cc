2734cc5e456870c8e497f78b6984f83d6a1ea9ff
Michael Clark <michael@metaparadigm.com>
2015-04-04 16:55:08 +0800

    uncomment close call that causes SSL_do_handshake to crash on subsequent connections

--- tests/openssl_async_echo_server.cc
+++ tests/openssl_async_echo_server.cc
@@ -239,7 +239,8 @@ int main(int argc, char **argv)
                 // TODO - crashes on subsequent connections in SSL_do_handshake if we close the fd.
                 //        ssl_lib.c::SSL_do_handshake::s->method->ssl_renegotiate_check(s);
                 //        Why? reuse of same fd number for subsequent connection?
-                // close(ssl_conn.conn_fd);
+                //        comment the following line and the server works but leaks fds
+                close(ssl_conn.conn_fd);
                 auto pi = std::find_if(poll_vec.begin(), poll_vec.end(), [conn_fd] (const struct pollfd &pfd){
                     return pfd.fd == conn_fd;
                 });
